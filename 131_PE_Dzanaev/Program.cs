2fad333f9751993772a683f6e1b12387b6a0210e
Dzanaev O. <robrenin@gmail.com>
2016-02-01 11:13:11 +0300

    Пустой проект

---
+++ 131_PE_Dzanaev/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131_PE_Dzanaev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
